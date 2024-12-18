87352e75c5f6111055b12088743b352f5a5f1fec
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2024-12-18 22:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-//complete this code
-class Person {}
+// Person class definition
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+
+  // Getter for age (optional, in case you want to access age)
+  get age() {
+    return this._age;
+  }
+}
+
+// Student subclass
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+// Teacher subclass
+class Teacher extends Person {
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
